05a25378d6b2a04b4bb69a1b1fda8878d02bf2ba
Roman Fedoniuk <zona0031256@gmail.com>
2022-12-30 23:00:14 +0200

    Create main menu, sidebar, main menu pages

---
+++ Models/ViewComponents/SidebarViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyCompany.Domain;
+using System.Threading.Tasks;
+
+namespace MyCompany.Models.ViewComponents
+{
+    public class SidebarViewComponent : ViewComponent
+    {
+        private readonly DataManager dataManager;
+
+        public SidebarViewComponent(DataManager dataManager)
+        {
+            this.dataManager = dataManager;
+        }
+
+        public Task<IViewComponentResult> InvokeAsync()
+        {
+            return Task.FromResult((IViewComponentResult)View("Default", dataManager.ServiceItems.GetServiceItems()));
+        }
+    }
+}
